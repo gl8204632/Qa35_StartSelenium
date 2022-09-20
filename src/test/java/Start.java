2cc2f9179d6986d403a3b5a5b6bf1f7a6cbb35a3
gl8204632 <gl8204632@gmail.com>
2022-09-20 23:17:23 +0300

    added class Start

---
+++ src/test/java/Start.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chromium.ChromiumDriver;
+import org.testng.annotations.Test;
+
+public class Start {
+    WebDriver wd;
+    @Test
+    public void goToFhonebook(){
+        // open browser
+        wd= new ChromeDriver();
+
+        // go to phonebook
+
+        //1 way without history
+        //wd.get("https://contacts-app.tobbymarshall815.vercel.app/");
+
+        //2 way with history
+        wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/");
+
+        wd.navigate().back();
+        wd.navigate().forward();
+        wd.navigate().refresh();
+
+
+        // close browser
+        //1 way close only 1 tab
+        //wd.close();
+
+        //2 way close all tabs+browser
+        wd.quit();
+
+    }
+}
