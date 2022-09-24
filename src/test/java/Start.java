c634348e3e14cde24008ed8eb711a7a7c30c7f6d
gl8204632 <gl8204632@gmail.com>
2022-09-24 15:25:20 +0300

    findElement

--- src/test/java/Start.java
+++ src/test/java/Start.java
@@ -1,7 +1,12 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chromium.ChromiumDriver;
 import org.testng.annotations.Test;
+
+import java.util.List;
+
 
 public class Start {
     WebDriver wd;
@@ -36,8 +41,31 @@ public class Start {
     public void LoginTest(){
         wd=new ChromeDriver();
         wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/");
-        //
-        //
+        // open LoginPage: find LoginTab+click
+        // fill email: find ElementEmail + type "email"
+        // login submit : find login button + click
+        // assert (ex result= actual result)
+
+
+        // h1 --- PHONEBOOK; Home Component
+        // way 1 finding only first element h1
+        WebElement element= wd.findElement(By.tagName("h1"));
+        element.click();
+
+        List<WebElement> list=  wd.findElements(By.tagName("h1"));
+       list.get(1); // finding webElement "HOMECOMPONENT" with index 1 , second element in collection List
+        WebElement element1 = list.get(1); //HOMECOMPONENT
+       element1.click();
+
+
+
+        // by id
+        wd.findElement(By.id("root"));
+        //by class
+        wd.findElement(By.className("container"));
+
+
+
         wd.quit();
     }
 
