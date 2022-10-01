67d32d678882861699eedb3ae428a33641087f50
gl8204632 <gl8204632@gmail.com>
2022-10-01 17:40:05 +0300

    Homework1

--- src/test/java/Start.java
+++ src/test/java/Start.java
@@ -69,8 +69,8 @@ public class Start {
         wd.findElement(By.partialLinkText("AB"));
 
         //by name
-        //wd.findElement(By.name("name"));
-       // wd.findElement(By.name("surename"));
+        //wd.findElement(By.name("name"));//??????????????
+       // wd.findElement(By.name("surename"));//?????????????
 
         //by cssSelector
         // by tagname
@@ -86,7 +86,7 @@ public class Start {
         //by attribute
         wd.findElement(By.cssSelector("[href='/home']"));
         wd.findElement(By.cssSelector("[href ^='/ho']"));   // starts with "ho"
-       wd.findElement(By.cssSelector("[href *='om']"));     //containce "om"
+        wd.findElement(By.cssSelector("[href *='om']"));     //containce "om"
         wd.findElement(By.cssSelector("[href $='me']"));    // ends of "me"
 
         wd.findElement(By.cssSelector("[href]"));
@@ -104,12 +104,55 @@ public class Start {
 
 
 
-        wd.quit();
+
     }
 @Test
     public void homework (){
     wd= new ChromeDriver();
-        wd.navigate().to("file://Users//tayahatum/downloads/apk/index.htlm");
+        wd.navigate().to("file:///C:/Users/Larisa/Downloads/index%20(1).html");
+
+        //ITEM1
+
+        WebElement item1 = wd.findElement(By.cssSelector("a[href='#item1']"));
+        //WebElement item1_1 = wd.findElement(By.linkText("item1"));//?????????????
+        WebElement item1_2 = wd.findElement(By.cssSelector("[href $='m1']"));
+        WebElement item1_3 = wd.findElement(By.tagName("a"));
+        List<WebElement> list1 = wd.findElements(By.tagName("a"));
+        WebElement item1_4= list1.get(0);
+        WebElement item1_5 = (wd.findElements(By.tagName("a"))).get(0);
+        WebElement item1_6 = wd.findElement(By.className("nav-item"));
+
+        //ITEM2 and the others the same
+
+    WebElement item2 = wd.findElement(By.cssSelector("a[href='#item2']"));
+    //WebElement item2_1 = wd.findElement(By.linkText("item2"));//?????????????
+    WebElement item2_2 = wd.findElement(By.cssSelector("[href $='m2']"));
+    WebElement item2_3 = wd.findElement(By.tagName("a"));
+    //List<WebElement> list1 = wd.findElements(By.tagName("a"));
+    WebElement item2_4= list1.get(1);
+    WebElement item2_5 = (wd.findElements(By.tagName("a"))).get(1);
+    WebElement item2_6 = wd.findElement(By.className("nav-item"));
+
+
+        // FIND ELEMENT OF FORM
+
+    WebElement name = wd.findElement(By.name("name"));
+    WebElement name1 =wd.findElement(By.cssSelector("[placeholder='Type your name']"));
+    WebElement surename = wd.findElement(By.name("surename"));
+    WebElement surename1 =wd.findElement(By.cssSelector("[placeholder='Type your surename']"));
+
+    WebElement send1 = wd.findElement(By.className("btn"));
+    WebElement send2 = wd.findElement(By.cssSelector(".btn"));
+
+        //FIND FROM TABLE
+
+    List<WebElement> list2 = wd.findElements(By.tagName("td"));
+    WebElement e4 = list2.get(9);
+    String text = e4.getText();
+    System.out.println(text);
+
+    wd.quit();
+
 
     }
 }
